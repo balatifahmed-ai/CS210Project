ebbe2ed703f0883796793da048b015ebec80ac2f
balatifahmed-ai <balatifahmed@gmail.com>
2025-10-28 20:26:19 +0300

    Hello

---
+++ src/main/java/cs/twoten/project/Main.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package cs.twoten.project;
+
+/**
+ *
+ * @author aaaab
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 3; i++) {
+            System.out.println(args[i]);
+        }
+    }
+}

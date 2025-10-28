7c28f91fa4ada8616b3bf0227acd5b1ffaa18a81
balatifahmed-ai <balatifahmed@gmail.com>
2025-10-28 20:43:46 +0300

    Finished AVLNode

---
+++ src/main/java/cs/twoten/project/AVLNode.java
@@ -0,0 +1,19 @@
+package cs.twoten.project;
+
+public class AVLNode {
+    String token;
+    int height;
+    int frequency;
+    SList List;
+    AVLNode left;
+    AVLNode right;
+
+    public AVLNode (String token,int height, int frequency, SList List, AVLNode left, AVLNode right) {
+        this.token = token;
+        this.height = height;
+        this.frequency = frequency;
+        this.List = List;
+        this.left = left;
+        this.right = right;
+    }
+}
